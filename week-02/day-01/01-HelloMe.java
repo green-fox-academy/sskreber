fcd7c569ac3934e78868f84a42948e07a015e19b
sskreber <zsu.karap@gmail.com>
2018-04-08 12:49:30 +0200

    Fixed 01-HelloMe rewrite

--- week-02/day-01/01-HelloMe.java
+++ week-02/day-01/01-HelloMe.java
@@ -1,7 +1,7 @@
-
+﻿
 public class HelloOthers {
     public static void main(String[] args) {
-        // I am greeting 3 of your classmates with this program, in three separate lines
-        System.out.println("Hey Ági!" + "\n" + "Konbanwa DD!" + "\n" + "Hi Anita!");
+        // A Hello World alternative
+        System.out.println("Hello fantastic software dev fancying person aka Me!");
     }
 }
