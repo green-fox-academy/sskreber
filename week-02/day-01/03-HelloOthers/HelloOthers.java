ca686f43d78d4af7075df4c45e35e3c611247c75
sskreber <zsu.karap@gmail.com>
2018-04-08 03:53:07 +0200

    Added 03-HelloOthers

---
+++ week-02/day-01/03-HelloOthers/HelloOthers.java
@@ -0,0 +1,6 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // I am greeting 3 of your classmates with this program, in three separate lines
+        System.out.println("Hey Ági!" + "\n" + "Konbanwa DD!" + "\n" + "Hi Anita!");
+    }
+}
