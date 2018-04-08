ac8302db0c66b0dd16b49a418e071d0ebec0b466
sskreber <zsu.karap@gmail.com>
2018-04-08 04:00:26 +0200

    Deleted folders, made tasks files only

---
+++ week-02/day-01/01-HelloMe.java
@@ -0,0 +1,7 @@
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // I am greeting 3 of your classmates with this program, in three separate lines
+        System.out.println("Hey Ági!" + "\n" + "Konbanwa DD!" + "\n" + "Hi Anita!");
+    }
+}
--- week-02/day-01/01-HelloMe/src/HelloMe.java
+++
@@ -1,5 +0,0 @@
-public class HelloMe {
-    public static void main(String[] args) {
-        System.out.println("Hello, you fantastic almost junior-developer person aka Me!");
-    }
-}
