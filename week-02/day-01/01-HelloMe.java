019582e43c926be75edc153281e6301efd162dc1
sskreber <zsu.karap@gmail.com>
2018-04-08 12:51:02 +0200

    Fixed 01-HelloMe class name

--- week-02/day-01/01-HelloMe.java
+++ week-02/day-01/01-HelloMe.java
@@ -1,5 +1,5 @@
 ﻿
-public class HelloOthers {
+public class HelloMe {
     public static void main(String[] args) {
         // A Hello World alternative
         System.out.println("Hello fantastic software dev fancying person aka Me!");
