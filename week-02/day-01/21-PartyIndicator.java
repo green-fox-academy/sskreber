ef181db2a8fb818515c978f1547c46e807fcf009
sskreber <zsu.karap@gmail.com>
2018-04-08 18:53:05 +0200

    Added 21-PartyIndicator

---
+++ week-02/day-01/21-PartyIndicator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PartyIndicator {
+    public static void main(String[] args) {
+        // Write a program that asks for two numbers
+        // Thw first number represents the number of girls that comes to a party, the
+        // second the boys
+        // It should print: The party is exellent!
+        // If the the number of girls and boys are equal and there are more people coming than 20
+        //
+        // It should print: Quite cool party!
+        // It there are more than 20 people coming but the girl - boy ratio is not 1-1
+        //
+        // It should print: Average party...
+        // If there are less people coming than 20
+        //
+        // It should print: Sausage party
+        // If no girls are coming, regardless the count of the people
+
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber1 = scanner.nextInt();
+
+//        Scanner scanner = new Scanner(System.in);
+        int inputNumber2 = scanner.nextInt();
+
+        if ((inputNumber1 == inputNumber2) && (inputNumber1 + inputNumber2 > 20)) {
+            System.out.println("The party is excellent!");
+        } else if ((inputNumber1 != inputNumber2) && (inputNumber1 + inputNumber2 > 20)) {
+            // Mind you, the quite a cool party condition will also check true if 0 girls are coming but there are more than 20 guys!
+            // (Because the 0:100 ratio is NOT 1:1 either...)
+            System.out.println("Quite a cool party!");
+        } else if (inputNumber1 + inputNumber2 < 20) {
+            System.out.println("Average party...");
+        } else if (inputNumber1 == 0) {
+            System.out.println("Sausage party");
+            // The above condition will only check true if 0 girls and EXACTLY 20 guys are present.
+            // If there are less than 20, the "average party" condition will kick in, and if there are more than 20, the "quite a cool party" will.
+        }
+    }
+}
