e402085abc99cc767051e180cdd2104b8aeccc31
sskreber <zsu.karap@gmail.com>
2018-04-08 16:26:23 +0200

    Added 07-FavoriteNumber

---
+++ week-02/day-01/07-FavoriteNumber.java
@@ -0,0 +1,9 @@
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        // Storing favorite number in a variable (as a number)
+        // And printing it like this: "My favorite number is: 8"
+
+        int favoriteNumber = 6;
+        System.out.println("My favourite number is: " + favoriteNumber);
+    }
+}
