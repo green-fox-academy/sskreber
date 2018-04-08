b27f0841cbb2b5319843ae546ebe129e228de156
sskreber <zsu.karap@gmail.com>
2018-04-08 03:55:35 +0200

    Fixed typo in 03-HelloOthers.java

--- week-02/day-01/03-HelloOthers/HelloOthers.java
+++ week-02/day-01/03-HelloOthers/HelloOthers.java
@@ -1,6 +1,6 @@
 public class HelloOthers {
     public static void main(String[] args) {
-        // I am greeting 3 of your classmates with this program, in three separate lines
-        System.out.println("Hey Ági!" + "\n" + "Konbanwa DD!" + "\n" + "Hi Anita!");
+        // I am greeting 3 of my classmates with this program, in three separate lines
+        System.out.println("Hey Agi!" + "\n" + "Konbanwa DD!" + "\n" + "Hi Anita!");
     }
 }
