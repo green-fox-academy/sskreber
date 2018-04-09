2232d65535b5404d4a7d136c865f8dcccb7b82f8
sskreber <zsu.karap@gmail.com>
2018-04-09 20:58:39 +0200

    Added name prompt to 14-HelloUser

--- week-02/day-01/14-HelloUser.java
+++ week-02/day-01/14-HelloUser.java
@@ -5,6 +5,7 @@ public class HelloUser {
         // Modify this program to greet user instead of the World!
         // The program should ask for the name of the user
 
+        System.out.println("What's your name?");
         Scanner scanner = new Scanner (System.in);
         String userName = scanner.nextLine();
 
