e451839d03ade9167e47ed78c736307db537fdfc
sskreber <zsu.karap@gmail.com>
2018-04-09 19:23:48 +0200

    Fixed 09-Bmi

--- week-02/day-01/09-Bmi.java
+++ week-02/day-01/09-Bmi.java
@@ -1,4 +1,4 @@
-public class Bmi {
+public class stringTutorial {
     public static void main(String[] args) {
         double massInKg = 81.2;
         double heightInM = 1.78;
@@ -8,11 +8,5 @@ public class Bmi {
 
         double bmi = massInKg / (Math.pow(heightInM, 2));
         System.out.println(bmi);
-
-        if (true) {
-            System.out.println(bmi);
-        } else (false){
-            System.out.println(bmi);
-        }
     }
 }
