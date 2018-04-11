c722788f8240749a5b897b1c1f758575c5a5aa66
sskreber <zsu.karap@gmail.com>
2018-04-11 22:07:07 +0200

    Added 27-FizzBuzz-version02

---
+++ week-02/day-01/27-FizzBuzz-version02.java
@@ -0,0 +1,23 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        // Write a program that prints the numbers from 1 to 100.
+        // But for multiples of three print “Fizz” instead of the number
+        // and for the multiples of five print “Buzz”.
+        // For numbers which are multiples of both three and five print “FizzBuzz”.
+
+        for (int i = 1; i <= 100; i++){
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz"); // when a number has neither 3 nor 5 as its divider.
+            }
+            else if (i % 3 == 0){
+                System.out.println("Fizz"); // when the number can be divided by 3 without a remainder but not by 5
+            }
+            else if (i % 5 == 0){
+                System.out.println("Buzz"); // when the number can be divided by 5 without a remainder but not by 3
+            }
+            else {
+                System.out.println(i); // when the number can be divided by both 3 and 5 without a remainder
+            }
+        }
+    }
+}
