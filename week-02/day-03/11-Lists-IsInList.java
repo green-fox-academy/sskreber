d26bf08a61259ee1ad6e14724f395dcbc94334a2
sskreber <zsu.karap@gmail.com>
2018-04-13 14:09:21 +0200

    Added 11-Lists-IsInList

---
+++ week-02/day-03/11-Lists-IsInList.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class IsInList {
+    public static void main(String... args) {
+        // ArrayList<Integer> list = (ArrayList)Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16);
+        /* sskreber: kept getting errors for the above line of the original code,
+        so I commented it out.
+         */
+
+        // sskreber: added the following two extra lines instead of the one above, to initialise arraylist & give it its content.
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
+
+        checkNums(list);
+        // Check if list contains all of the following elements: 4,8,12,16
+        // Create a method that accepts list as an input
+        // it should return "true" if it contains all, otherwise "false"
+    }
+
+    public static boolean checkNums(ArrayList<Integer> anyList) {
+        boolean containsAll = true;
+        ArrayList<Integer> listToContain = new ArrayList<>();
+        listToContain.addAll(Arrays.asList(4, 8, 12, 16));
+        if (!anyList.containsAll(listToContain)) {
+            containsAll = false;
+        }
+        System.out.println(containsAll);
+        return containsAll;
+    }
+}
