fc297b54a81fda0e27fdb1e2013bd36ccecdc939
sskreber <zsu.karap@gmail.com>
2018-04-16 13:02:59 +0200

    Added 03-Functions-AppendAFunc

---
+++ week-03/day-02/03-Functions.AppendAFunc.java
@@ -0,0 +1,20 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `am` and assign the value `kuty` to it
+// - Write a function called `appendA` that gets a string as an input,
+//   appends an 'a' character to its end and returns with a string
+//
+// - Print the result of `appendAFunc(am)`
+
+    public class AppendAFunc {
+    public static void main(String[] args) {
+        String am = "kuty";
+        System.out.println(appendA(am));
+    }
+
+    public static String appendA(String anyStringVar) {
+        anyStringVar += "a";
+        return anyStringVar;
+    }
+}
