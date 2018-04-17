1a188de96483597222d70d564ed728eb1fc60343
sskreber <zsu.karap@gmail.com>
2018-04-17 23:14:12 +0200

    Added 17-HardOnes-SubStrList, renamed directories

---
+++ week-02/day-03-Strings_and_arraylists/17-HardOnes-SubStrList (Find the substring in the list).java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SubStrList {
+    public static void main(String[] args) {
+        //  Create a function that takes a string and a list of string as a parameter
+        //  Returns the index of the string in the list where the first string is part of
+        //  Only need to find the first occurrence and return the index of that
+        //  Returns `-1` if the string is not part any of the strings in the list
+
+        //  Example
+        String[] searchArr = new String[]{"this", "is", "what", "I'm", "searching"};
+        System.out.println(subStrList("ching", searchArr));
+        //  should print: `4`
+        System.out.println(subStrList("not", searchArr));
+        //  should print: `-1`
+    }
+
+    public static int subStrList(String anyString, String[] stringArray) {
+        for (String s : stringArray) {
+            if (s.contains(anyString)) {
+                return Arrays.asList(stringArray).indexOf(s);
+            }
+        }
+        return -1;
+    }
+}
