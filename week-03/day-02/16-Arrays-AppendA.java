f62f41ddd8f529f5bbd0245fc60df5c279da705a
sskreber <zsu.karap@gmail.com>
2018-04-17 18:49:25 +0200

    Added 16-Arrays-AppendA, renamed 14-Arrays-DoubleItems

---
+++ week-03/day-02/16-Arrays-AppendA.java
@@ -0,0 +1,13 @@
+// - Create an array variable named `nimals`
+//   with the following content: `["kuty", "macsk", "cic"]`
+// - Add all elements an `"a"` at the end
+
+public class AppendA {
+    public static void main(String[] args) {
+        String[] nimals = new String[]{"kuty", "macs", "cic"};
+        for (int i = 0; i < nimals.length; i++) {
+            nimals[i] += "a";
+            System.out.println(nimals[i]);      // just to check updated elements
+        }
+    }
+}
