1e1ceaa34ef0a0a335b9d4806ec7a2996ee5a8e1
sskreber <zsu.karap@gmail.com>
2018-04-20 15:41:32 +0200

    Added 07-FourRectangles & 08-PositionSquare

---
+++ week-03/day-03-Drawing/07-FourRectangles.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+
+    public static void mainDraw(Graphics graphics) {
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+
+        /* sskreber: was thinking of creating an ArrayList<String> with colour names, pulling out
+        items of it randomly (with Math.random() on its indeces) and feeding it into the
+        graphics.setColor command to avoid calling latter 4 times, but it doesn't accept String variables
+        as an argument... How to avoid calling graphics.setColor 4 times then?... TBC.
+         */
+
+        for (int i = 0; i < 4; i++) {
+            if (i == 0) {
+                graphics.setColor(Color.GREEN);
+            } else if (i == 1) {
+                graphics.setColor(Color.RED);
+            } else if (i == 2) {
+                graphics.setColor(Color.BLUE);
+            } else {
+                graphics.setColor(Color.BLACK);
+            }
+            int rectX = (int) (Math.random() * (WIDTH));
+            int rectY = (int) (Math.random() * (HEIGHT));
+            int rectWidth = (int) (Math.random() * (WIDTH));
+            int rectHeight = (int) (Math.random() * (HEIGHT));
+            graphics.drawRect(rectX, rectY, rectWidth, rectHeight);
+        }
+    }
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
---
+++ week-03/day-03-Drawing/08-PositionSquare.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+
+    public static void mainDraw(Graphics graphics) {
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+        
+        /* sskreber: TBC: don't know how to NOT pass graphcis as a parameter to the new method, 
+        as far as I see no drawing method can function without receiving a Graphics argument
+        (won't recognise graphics.setColor(), graphics.drawRect(), etc otherwise).
+         */
+
+        for (int i = 0; i < 3; i++) {
+            int squareX = (int) (Math.random() * (WIDTH + 1));
+            int squareY = (int) (Math.random() * (HEIGHT + 1));
+            positionSquare(graphics, squareX, squareY);
+        }
+    }
+
+    public static void positionSquare(Graphics graphics, int squareX, int squareY) {
+        int rectWidthHeight = (int) (Math.random() * (WIDTH));
+        graphics.drawRect(squareX, squareY, rectWidthHeight, rectWidthHeight);
+    }
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
