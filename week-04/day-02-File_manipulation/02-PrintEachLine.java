5cbc3e0b90436dc6d0972585311060a1cf3427a2
sskreber <zsu.karap@gmail.com>
2018-04-25 20:23:19 +0200

    Fixing 02-PrintEachLine

--- week-04/day-02-File_manipulation/02-PrintEachLine.java
+++ week-04/day-02-File_manipulation/02-PrintEachLine.java
@@ -1,4 +1,4 @@
-﻿
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,10 +15,12 @@ public class PrintEachLine {
         try {
             Path file = Paths.get("my-file.txt");
             List<String> lines = Files.readAllLines(file);
-            System.out.println(lines);
+            for (String line:
+                 lines) {
+                System.out.println(line);
+            }
         } catch (IOException e) {
             System.out.println("Unable to read file: my-file.txt");
-            System.out.println("java's family-friendly built-in error message á la Derek Banas: " + e.getMessage());
             e.printStackTrace();    // prints the standard java error
             System.out.println(e.toString());   // gives the actual exception name
         }
