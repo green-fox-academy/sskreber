e95889b158d9a37c6cc7bcb15e74eb9cc267b4a0
sskreber <zsu.karap@gmail.com>
2018-04-25 09:56:21 +0200

    Add 01-DivideByZero

---
+++ week-04/day-02-File_manipulation/01-DivideByZero.java
@@ -0,0 +1,24 @@
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print "fail" if the parameter is 0
+
+import java.util.Scanner;
+
+public class DivideByZero {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a whole number: ");
+        int inputDivisor = scanner.nextInt();
+
+        try {
+            int result = 10 / inputDivisor;
+            System.out.println("The result is: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("fail");
+        }
+    }
+}
