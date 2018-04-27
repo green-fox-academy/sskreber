b4b8bd1f8c095d7136f93c41a5502f72a7eda4cf
sskreber <zsu.karap@gmail.com>
2018-04-27 20:14:53 +0200

    Adding DoableHomework

---
+++ week-05/day-01-Object_oriented_programming/00-DoableHomework.java
@@ -0,0 +1,23 @@
+//Create 5 trees
+//        Store the data of them in variables in your program
+//        for every tree the program should store its'
+//        type
+//        leaf color
+//        age
+//        sex
+//        you can use just variables, or lists and/or maps
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DoableHomework {
+
+    public static void main(String[] args) {
+        ArrayList<String> evergreen = new ArrayList<>(Arrays.asList("evergreen", "green leaf", "1 year", "unisex"));
+        ArrayList<String> appleTree = new ArrayList<>(Arrays.asList("apple tree", "light green leaf", "2 years", "female"));
+        ArrayList<String> larch = new ArrayList<>(Arrays.asList("larch", "brownish green", "3 years", "female"));
+        ArrayList<String> cocconutTree = new ArrayList<>(Arrays.asList("cocconut tree", "fresh green", "2 years", "female"));
+        ArrayList<String> cherryTree = new ArrayList<>(Arrays.asList("cherry tree", "light pink", "1 year", "unisex"));
+    }
+
+}
