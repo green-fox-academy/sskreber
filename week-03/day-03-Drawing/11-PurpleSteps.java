70237d7673f8dddbb3d096f44f79330d395d2792
sskreber <zsu.karap@gmail.com>
2018-05-06 20:16:26 +0200

    Adding 11-PurpleSteps

---
+++ week-03/day-03-Drawing/11-PurpleSteps.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps {
+    public static void mainDraw(Graphics graphics) {
+        // Reproduce this:
+        // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+        int squareSize = 10;
+        graphics.setColor(new Color(153, 1, 153));
+        for (int i = 1; i < 20; i++) {
+            graphics.fillRect(squareSize * i, squareSize * i, squareSize, squareSize);
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
+   
