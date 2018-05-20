d12f1bf624d0e75b5f789d68eb18a63bfe734b97
sskreber <zsu.karap@gmail.com>
2018-05-20 18:33:20 +0200

    Adding Exercise2

---
+++ week-07/day-03-Stream/Exercise2.java
@@ -0,0 +1,18 @@
+//Write a Stream Expression to get the average value of the odd numbers from the following array:
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+        double average = numbers.stream()
+                .filter(n -> Math.abs(n % 2) == 1)
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+
+        System.out.println(average);
+    }
+}
+
