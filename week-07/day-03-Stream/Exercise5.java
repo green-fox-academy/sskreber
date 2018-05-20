14f508725a247d3fc51247147ba64c4f2135483b
sskreber <zsu.karap@gmail.com>
2018-05-20 18:55:29 +0200

    Adding Exercise5

---
+++ week-07/day-03-Stream/Exercise5.java
@@ -0,0 +1,18 @@
+//Write a Stream Expression to find the uppercase characters in a string!
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+
+        String whateverWord = "jhbjhTZHgbnGhThfzgR";
+        List<Character> whateverWordUppercases = whateverWord.chars()
+                .filter(n -> n > 64 && n < 91)
+                .mapToObj(n -> (char) n)
+                .collect(Collectors.toList());
+
+        System.out.println(whateverWordUppercases);
+    }
+}
+
