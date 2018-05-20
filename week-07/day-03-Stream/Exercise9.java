e26e56e8d2be57f4368529086840364b518f0177
sskreber <zsu.karap@gmail.com>
2018-05-20 20:08:33 +0200

    Adding Exercise9

---
+++ week-07/day-03-Stream/Exercise9.java
@@ -0,0 +1,17 @@
+//Write a Stream Expression to convert a char array to a string!
+
+import java.util.stream.Collectors;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        char[] charArray = {'g', 'o', 'o', 'd', 'i', 'e'};
+
+        String charArrayStringVersion = new String(charArray).chars()
+                .mapToObj(n -> (char) n)
+                .map(n -> n.toString())
+                .collect(Collectors.joining());
+
+        System.out.println(charArrayStringVersion);
+    }
+}
+
