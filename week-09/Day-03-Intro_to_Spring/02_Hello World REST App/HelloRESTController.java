aa4b410490ba1b93d5650bfba0e90f070d0c5630
sskreber <zsu.karap@gmail.com>
2018-06-03 17:18:03 +0200

    Adding 02_Hello World REST App (single file -not-commented-out version-)

--- week-09/Day-03-Intro_to_Spring/02_Hello World REST App/HelloRESTController.java
+++ week-09/Day-03-Intro_to_Spring/02_Hello World REST App/HelloRESTController.java
@@ -1,15 +1,15 @@
-//package com.greenfoxacademy.springstart.controllers;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class HelloRESTController {
-//    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-//    public Greeting greet() {
-//        return new Greeting(1, "Hello World!");
-//    }
-//}
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRESTController {
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public Greeting greet() {
+        return new Greeting(1, "Hello World!");
+    }
+}
 
