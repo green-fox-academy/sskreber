5ab50cd1d6e91ccc645a7daaa670124c9268b3e1
sskreber <zsu.karap@gmail.com>
2018-06-20 20:23:19 +0200

    Adding login function, lets user log in with name, uses it in url

--- week-12/day-02-TTC/programmer-fox-club/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
+++ week-12/day-02-TTC/programmer-fox-club/src/main/java/com/greenfoxacademy/programmerfoxclub/controllers/MainController.java
@@ -1,13 +1,30 @@
 package com.greenfoxacademy.programmerfoxclub.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping(value = "/index")
-    public String indexPage() {
+    @GetMapping("/")
+    public String indexPage(@RequestParam(value = "name", required = false) String name) {
+        if (name == null) {
+            return "login";
+        }
         return "index";
+    }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping(value = "/submitname")
+    public String login(@RequestParam(value = "name") String name) {
+        return "redirect:/?name=" + name;
     }
 }
