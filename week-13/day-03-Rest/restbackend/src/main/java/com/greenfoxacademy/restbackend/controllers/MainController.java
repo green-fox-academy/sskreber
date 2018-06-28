1894638fb7e3c72717abdeade64b089f4300d7de
sskreber <zsu.karap@gmail.com>
2018-06-28 22:10:11 +0200

    Init project(RestBackend), feat(MainController)

---
+++ week-13/day-03-Rest/restbackend/src/main/java/com/greenfoxacademy/restbackend/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.restbackend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = "/")
+    public String renderIndex() {
+        return "index";
+    }
+}
