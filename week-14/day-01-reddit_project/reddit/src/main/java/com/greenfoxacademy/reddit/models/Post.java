3eb27d25f5047f914e7c64a21034e66e0e20e696
sskreber <zsu.karap@gmail.com>
2018-07-03 14:44:40 +0200

    feat(init): project, feat: Post class

---
+++ week-14/day-01-reddit_project/reddit/src/main/java/com/greenfoxacademy/reddit/models/Post.java
@@ -0,0 +1,78 @@
+package com.greenfoxacademy.reddit.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String title;
+    private String url;
+    private Timestamp timestamp;
+    private Long score;
+
+    public Post() {
+    }
+
+    public Post(String title, String url, Timestamp timestamp, Long score) {
+        this.title = title;
+        this.url = url;
+        this.timestamp = timestamp;
+        this.score = score;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+}
