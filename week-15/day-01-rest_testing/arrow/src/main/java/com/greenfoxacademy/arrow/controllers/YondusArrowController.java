ff122e50d276b16073ed2f0972e46f30121d564e
sskreber <zsu.karap@gmail.com>
2018-07-10 21:01:36 +0200

    feat: add YondusArrowController

--- week-15/day-01-rest_testing/arrow/src/main/java/com/greenfoxacademy/arrow/controllers/YondusArrowController.java
+++ week-15/day-01-rest_testing/arrow/src/main/java/com/greenfoxacademy/arrow/controllers/YondusArrowController.java
@@ -1,10 +1,23 @@
 package com.greenfoxacademy.arrow.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.greenfoxacademy.arrow.models.*;
 
 @RestController
 public class YondusArrowController {
-    
+
+    @GetMapping("/yondu")
+    public ResponseEntity<YondusArrowController> getSpeedUsingUserInput(@RequestParam(value = "distance", required = false) Double distance,
+    @RequestParam(value = "time", required = false) Double time) {
+        if (distance != null && time != null) {
+            return new ResponseEntity(new Speed(distance, time), HttpStatus.OK);
+        } else {
+            return new ResponseEntity(new ErrorMessage("No input given. Please enter distance and time."), HttpStatus.BAD_REQUEST);
+
+        }
+    }
 }
--- week-15/day-01-rest_testing/arrow/src/main/java/com/greenfoxacademy/arrow/services/YondusArrowService.java
+++ week-15/day-01-rest_testing/arrow/src/main/java/com/greenfoxacademy/arrow/services/YondusArrowService.java
@@ -1,4 +1,7 @@
 package com.greenfoxacademy.arrow.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class YondusArrowService {
 }
