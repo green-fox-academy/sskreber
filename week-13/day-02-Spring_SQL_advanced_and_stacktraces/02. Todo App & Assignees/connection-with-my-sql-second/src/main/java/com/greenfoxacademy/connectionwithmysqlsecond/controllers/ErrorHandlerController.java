c93e25e4571d212e4eeb71a27e6880007ce94b74
sskreber <zsu.karap@gmail.com>
2018-08-12 19:27:52 +0200

    feat: ErrorHandlerController for general url typos, unfunctioning pic on error-page.html

---
+++ week-13/day-02-Spring_SQL_advanced_and_stacktraces/02. Todo App & Assignees/connection-with-my-sql-second/src/main/java/com/greenfoxacademy/connectionwithmysqlsecond/controllers/ErrorHandlerController.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy.connectionwithmysqlsecond.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorHandlerController implements ErrorController {
+
+    private static final String ERROR_PATH = "/error";
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+    @GetMapping(ERROR_PATH)
+    public String getErrorPage() {
+        return "error-page";
+    }
+}
