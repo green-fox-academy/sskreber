6f7061581d1252b1379d0ac9e58292d2fc4d40af
sskreber <zsu.karap@gmail.com>
2018-08-12 16:04:23 +0200

    feat: edit assignee option

--- week-13/day-02-Spring_SQL_advanced_and_stacktraces/02. Todo App & Assignees/connection-with-my-sql-second/src/main/java/com/greenfoxacademy/connectionwithmysqlsecond/controllers/AssigneeController.java
+++ week-13/day-02-Spring_SQL_advanced_and_stacktraces/02. Todo App & Assignees/connection-with-my-sql-second/src/main/java/com/greenfoxacademy/connectionwithmysqlsecond/controllers/AssigneeController.java
@@ -4,10 +4,14 @@ import com.greenfoxacademy.connectionwithmysqlsecond.models.Assignee;
 import com.greenfoxacademy.connectionwithmysqlsecond.models.Todo;
 import com.greenfoxacademy.connectionwithmysqlsecond.services.AssigneeService;
 import com.greenfoxacademy.connectionwithmysqlsecond.services.TodoService;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Controller
 public class AssigneeController {
@@ -41,6 +45,24 @@ public class AssigneeController {
     @PostMapping("assignee/delete/{id}")
     public String deleteAssignee(@PathVariable(value = "id") long id) {
         assigneeService.deleteAssignee(id);
+        return "redirect:/assignees";
+    }
+
+    @GetMapping("assignee/edit/{id}")
+    public String edit(@PathVariable(value = "id") long id, Model assigneeToUpdateModel) {
+
+        Optional<Assignee> assigneeToUpdate = assigneeService.getAssigneeById(id);
+        assigneeToUpdateModel.addAttribute("assigneeToUpdate", assigneeToUpdate.get());
+        assigneeToUpdateModel.addAttribute("id", id);
+        return "assignee-edit";
+    }
+
+    @PostMapping("assignee/update/{id}")
+    public String updateTodo(@PathVariable(value = "id") long id,
+                             @ModelAttribute(value = "assigneeToUpdate") Assignee assigneeToUpdate) {
+
+        assigneeToUpdate.setId(id);
+        assigneeService.updateAssignee(assigneeToUpdate);
         return "redirect:/assignees";
     }
 }
