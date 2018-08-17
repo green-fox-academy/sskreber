b2d6f9c225c23448278fa9914186233c3fa4b2b5
sskreber <zsu.karap@gmail.com>
2018-08-17 21:59:27 +0200

    feat(ErrorHandlingController, error-page.html): display error attributes

--- week-13/day-02-Spring_SQL_advanced_and_stacktraces/02. Todo App & Assignees/connection-with-my-sql-second/src/main/java/com/greenfoxacademy/connectionwithmysqlsecond/controllers/ErrorHandlerController.java
+++ week-13/day-02-Spring_SQL_advanced_and_stacktraces/02. Todo App & Assignees/connection-with-my-sql-second/src/main/java/com/greenfoxacademy/connectionwithmysqlsecond/controllers/ErrorHandlerController.java
@@ -1,13 +1,25 @@
 package com.greenfoxacademy.connectionwithmysqlsecond.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
 
 @Controller
 public class ErrorHandlerController implements ErrorController {
 
     private static final String ERROR_PATH = "/error";
+    private ErrorAttributes errorAttributes;
+
+    @Autowired
+    public void setErrorAttributes(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
 
     @Override
     public String getErrorPath() {
@@ -15,7 +27,15 @@ public class ErrorHandlerController implements ErrorController {
     }
 
     @GetMapping(ERROR_PATH)
-    public String getErrorPage() {
+    public String getErrorPage(Model model, WebRequest webReq) {
+        Map<String, Object> errorMap = this.errorAttributes.getErrorAttributes(webReq, true);
+
+        model.addAttribute("timestamp", errorMap.get("timestamp"));
+        model.addAttribute("error", errorMap.get("error"));
+        model.addAttribute("message", errorMap.get("message"));
+        model.addAttribute("path", errorMap.get("path"));
+        model.addAttribute("status", errorMap.get("status"));
+
         return "error-page";
     }
 }
