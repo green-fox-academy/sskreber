7c414c9a5b8ab3e390a1f334bea11835564009d9
sskreber <zsu.karap@gmail.com>
2018-08-22 16:21:15 +0200

    style(SuperSecureController): modify root endpoint welcome message. refactor(here): remove unused import

--- week-20-Exam-projects/basic-security/src/main/java/com/greenfoxacademy/basicsecurity/controllers/SuperSecureController.java
+++ week-20-Exam-projects/basic-security/src/main/java/com/greenfoxacademy/basicsecurity/controllers/SuperSecureController.java
@@ -2,7 +2,6 @@ package com.greenfoxacademy.basicsecurity.controllers;
 
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +9,7 @@ public class SuperSecureController {
 
     @GetMapping("/")
     public String getMainPage() {
-        return "Main page: literally anyone can access this. Well, congrats anyway.";
+        return "Literally anyone with a name and password can access this. Well, congrats anyway.";
     }
 
     @Secured("ROLE_USER")
